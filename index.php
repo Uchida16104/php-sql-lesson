e25fe1379bc81a6912149ac36991e6d97f51d0a4
HU <uho02741358@gmail.com>
2023-07-27 15:43:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
 $array = array(
-    $height => (150/100),
-    $weight => 65,
-    $bmi   => $weight/$height/$height,
+    "height" => round(150/100,1),
+    "weight" => 65,
+    "bmi"   => round(65/1.5/1.5,1),
 );
-var_dump($array[$bmi]);
+var_dump($array["bmi"]);
 ?>
