1166f9cd9d2b40bb7559210f9602a29d8aae62e1
HU <uho02741358@gmail.com>
2023-07-27 15:24:55 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 header("Content-Type: application/json; charset=utf-8");
 print 'printで文字を表示します。';
 echo 'echoで文字を表示します。';
-$height = 1.5;
+$height = round(150 / 100 , 1);
 $weight = 65;
-const BMI = $weight/pow($height,2);
+const BMI = $weight/pow($height , 2);
 print '身長:'.$height.'m';
 print '体重:'.$weight.'kg';
 echo 'BMI:'.BMI.'(kg/m/m)';
-
+$random = rand();
+echo $random;
 ?>
