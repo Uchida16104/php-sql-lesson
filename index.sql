2263f2d0560d6555757a8aac721a237cb0062d33
HU <uho02741358@gmail.com>
2023-08-06 11:53:01 +0900

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,7 +1,8 @@
 DROP DATABASE foo;
 CREATE DATABASE foo;
 USE foo;
-SHOW VARIABLES LIKE 'hostname';
+SHOW VARIABLES WHERE Variable_name = 'hostname';
+SHOW VARIABLES WHERE Variable_name = 'port';
 CREATE TABLE param (id INT(3), name VARCHAR(10), class VARCHAR(10), subject VARCHAR(10), score INT(3));
 delimiter //
 create procedure loop_insert(in maxNum int)
