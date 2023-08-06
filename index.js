a7b09891215a8f5b8b2767a70b7c879191ad32f4
HU <uho02741358@gmail.com>
2023-08-06 22:49:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
  
-var client = mysql.createClient({
+var connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "root_password",
   database: "foo"
 });
  
-client.connect(function(err) {
+connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected to MySQL DB!");
 });
